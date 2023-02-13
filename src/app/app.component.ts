ace102203e301bfe7d7eb1bff8af936fb933894a
Aymane n <aymane.elammar@gmail.com>
2023-02-13 11:45:31 +0100

    history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,7 +68,7 @@ export class AppComponent implements OnInit, OnDestroy {
     const sensor1 = 'live_record/sensor1'
     const sensor2 = 'live_record/sensor2'
     this.subscription = this.db.read<any>('history').subscribe((value) => {
-      this.history = Object.keys(value).map((key) => [Number(key), value[key]]);
+      this.history = Object.keys(value).map((key) => [Number(key), value[key]]).slice(-10);
     })
     this.subscription = this.db.readSync<Record>([sensor1, sensor2]).subscribe((value) => {
       this.data = {sensor1: value[0], sensor2: value[1]};
